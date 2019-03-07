f53c3cb56947c8c577d2906a6df7e7d0806a4429
Jeremy Woo <Jeremy.Woo@c3posttrade.com>
2019-03-08 08:10:28 +1300

    init deployment files

---
+++ deploy/create.bat
@@ -0,0 +1,3 @@
+kubectl apply -f  pv.yml
+kubectl apply -f  pvc.yml
+kubectl apply -f  mongodb.yml
---
+++ deploy/delete.bat
@@ -0,0 +1,3 @@
+kubectl delete pv mongo-pv-volume
+kubectl delete pvc task-pv-claim
+kubectl delete pod mongodb-pod
