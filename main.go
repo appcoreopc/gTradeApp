e07dec1e7ada0a1523d01303032bf57bf76b5f14
JeremyWoo <jeremy.woo@c3posttrade.com>
2019-03-11 09:21:27 +1300

    added support to insert data into mongodb

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, _ := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	errConnect := client.Connect(ctx)
+	if errConnect != nil {
+		fmt.Println("ops !! unable to connect ")
+	}
 
 	collection := client.Database("testing").Collection("numbers")
 
 	if collection != nil {
 		fmt.Print("collection created")
 	}
+
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159})
+
+	if err != nil {
+		fmt.Println("ops !! unable insert record ")
+	}
+
+	id := res.InsertedID
+
+	fmt.Println("inserted id ", id)
 
 }
